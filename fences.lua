81d4337a258a00e9dd3f4e4a57cc63ae5608b58e
jgordon510 <jgordon.oakland@gmail.com>
2024-08-26 20:42:12 -0700

    fence adjustments

--- fences.lua
+++ fences.lua
@@ -1,19 +1,21 @@
+--for now, we remove all fences except 
 for i, node in pairs(minetest.registered_nodes) do
     if string.match(node.name, "fence") then
-        
-        if node.name ~= "default:fence_wood"  and node.name ~= "default:fence_rail_wood"then
-            minetest.clear_craft({
-                output = node.name
-            })
+        --remove all default recipes for fences
+        minetest.clear_craft({
+            output = node.name
+        })
+        --non default fences get removed entirely
+        if node.name ~= "default:fence_wood"  and node.name ~= "default:fence_rail_wood" then
             minetest.unregister_item(node.name)
         else
+            --default fences get "apple" removed
             local desc = ""
             if string.match(node.name, "rail") then
                 desc = "Wood Fence Rail"
             else
                 desc = "Wood Fence"
             end
-            minetest.log(node.name)
             minetest.override_item(node.name,{
                 description = desc
             })
@@ -21,6 +23,7 @@ for i, node in pairs(minetest.registered_nodes) do
     end
 end
 
+--register recipes with logs instead of planks
 minetest.register_craft({
     type = "shaped",
     output = "default:fence_wood 4",
